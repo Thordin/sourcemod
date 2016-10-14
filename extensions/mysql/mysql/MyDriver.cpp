a0037defe2d804d66a585697356259579f93e950
thordin <thordin@users.noreply.github.com>
2016-10-14 15:16:38 -0700

    Set charset to utf8mb4

--- extensions/mysql/mysql/MyDriver.cpp
+++ extensions/mysql/mysql/MyDriver.cpp
@@ -112,6 +112,8 @@ MYSQL *Connect(const DatabaseInfo *info, char *error, size_t maxlength)
 	my_bool my_true = true;
 	mysql_options(mysql, MYSQL_OPT_RECONNECT, (const char *)&my_true);
 
+	mysql_options(mysql, MYSQL_SET_CHARSET_NAME, "utf8mb4");
+
 	if (info->host[0] == '/')
 	{
 		host = "localhost";
